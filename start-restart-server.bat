91cd424da85afb819a42143f28ae9227064c81e0
Tom Witkowski <dev.gummibeer@gmail.com>
2015-05-02 09:28:11 +0200

    folder moved

--- start-restart-server.bat
+++ start-restart-server.bat
@@ -6,4 +6,4 @@ echo Server is stopped now ... restarting
 timeout /t 1 /nobreak
 echo Starting Server
 cd C:\a3l_server_live
-start "arma3" /min /high "arma3server.exe" "-profiles=C:\a3l_server_live\a3l_server\config" "-BEPath=C:\a3l_server_live\a3l_server\battleye" "-config=C:\a3l_server_live\a3l_server\config\config.cfg" "-cfg=C:\a3l_server_live\a3l_server\config\basic.cfg" -port=2302 -world=empty  -noSound -nologs -malloc=tbbmalloc -nosplash -cpuCount=4 -maxmem=3047 "-mod=@extDB;@life_server"
+start "arma3" /min /high "arma3server.exe" "-profiles=C:\a3l_server_live\config" "-BEPath=C:\a3l_server_live\battleye" "-config=C:\a3l_server_live\config\config.cfg" "-cfg=C:\a3l_server_live\config\basic.cfg" -port=2302 -world=empty  -noSound -nologs -malloc=tbbmalloc -nosplash -cpuCount=4 -maxmem=3047 "-mod=@extDB;@life_server"
