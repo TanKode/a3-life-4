e9eb1c921285d2972774f2b8a4ff7a036d4110d3
Gonger <Gonger87@gmx.de>
2015-05-03 13:18:24 +0200

    Markt System Fehler Bearbeitet

--- mpmissions/Altis_Life.Altis/Functions.h
+++ mpmissions/Altis_Life.Altis/Functions.h
@@ -359,7 +359,8 @@ class Life_Client_Core
 		class vehicleGarage {};
 		class gangDeposit {};
 	};
-	class Market
+};
+    class Market
     	{
     		file = "core\market";
 
@@ -376,8 +377,5 @@ class Life_Client_Core
     		class marketGetRow;
     		class marketGetPriceRow;
     		class marketSetPriceRow;
-    	};
-    	};
-
     	};
 
